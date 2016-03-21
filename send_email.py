42a7dd4e5171307268588ad9f9e984ce04f59519
kmaldaner <kmaldaner@gmail.com>
2016-03-21 07:50:16 +0000

    cleaned up some code

--- send_email.py
+++ send_email.py
@@ -1,46 +1,37 @@
 def send_email(name):
 	import getpass
-#	end an HTML email with an embedded image and a plain text message for
-	# email clients that don't want to display the HTML.
 
 	from email.MIMEMultipart import MIMEMultipart
 	from email.MIMEText import MIMEText
 	from email.MIMEImage import MIMEImage
 
-	# Define these once; use them twice!
 	strFrom = 'kylacamera@gmail.com'
 	strTo = 'kmaldaner@gmail.com'
 
-	# Create the root message and fill in the from, to, and subject headers
 	msgRoot = MIMEMultipart('related')
 	msgRoot['Subject'] = 'Someone entered your home at ' + name[:-4]
 	msgRoot['From'] = strFrom
 	msgRoot['To'] = strTo
 	msgRoot.preamble = 'This is a multi-part message in MIME format.'
 
-	# Encapsulate the plain and HTML versions of the message body in an
-	# 'alternative' part, so message agents can decide which they want to display.
 	msgAlternative = MIMEMultipart('alternative')
 	msgRoot.attach(msgAlternative)
 
 	msgText = MIMEText('This is the alternative plain text message.')
 	msgAlternative.attach(msgText)
 
-	# We reference the image in the IMG SRC attribute by the ID we give it below
 	msgText = MIMEText("I've attached a photo of whoever entered", 'html')
 	msgAlternative.attach(msgText)
 
-	# This example assumes the image is in the current directory
 	fp = open(name, 'rb')
 	msgImage = MIMEImage(fp.read())
 	fp.close()
 
-	# Define the image's ID as referenced above
 	msgImage.add_header('Content-ID', '<image1>')
 	msgRoot.attach(msgImage)
 
 	password  = getpass.getpass()
-	# Send the email (this example assumes SMTP authentication is required)
+
 	import smtplib
 	smtp = smtplib.SMTP()
 	smtp.connect('smtp.gmail.com',587)
